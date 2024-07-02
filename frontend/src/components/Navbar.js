7c1e9d24b78ab34736cc2a83f293f5f0de7f2ba4
Thay-de-Oliveira <thay.l.de.oliveira@gmail.com>
2024-07-02 20:49:18 -0300

    Atualização barra superior

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,20 +1,33 @@
-// src/components/Navbar.js
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 const CustomNavbar = () => (
-  <Navbar style={{ backgroundColor: '#005699' }} variant="dark" expand="lg" className="fixed-top">
-    <Navbar.Brand as={Link} to="/home">
-      <img
-        src="/logoNavbar.png"
-        height="45"
-        width="45"
-        alt="Logo"
-      />
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar className="custom-navbar fixed-top" variant="dark" expand="lg">
+    <Nav className="navbar-left">
+      <Nav.Link as={Link} to="#" className="navbar-icon">
+        <FontAwesomeIcon icon={faBars} size="lg" />
+      </Nav.Link>
+    </Nav>
+    <Nav className="navbar-center">
+      <Navbar.Brand as={Link} to="/home">
+        <img
+          src="/logoNavbar.png"
+          height="45"
+          width="45"
+          alt="Logo"
+          className="navbar-logo"
+        />
+      </Navbar.Brand>
+    </Nav>
+    <Nav className="navbar-right">
+      <Nav.Link as={Link} to="/logout" className="navbar-icon">
+        <FontAwesomeIcon icon={faSignOutAlt} size="lg" />
+      </Nav.Link>
+    </Nav>
   </Navbar>
 );
 
