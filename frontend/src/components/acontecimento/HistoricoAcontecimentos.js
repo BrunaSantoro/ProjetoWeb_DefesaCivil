c2154b9a5f2e8937ae5a70db2fd1ab5f6d68099d
Thay-de-Oliveira <thay.l.de.oliveira@gmail.com>
2024-07-06 21:02:19 -0300

    Botão de ver, editar e deletar funcionando em acontecimento

--- frontend/src/components/acontecimento/HistoricoAcontecimentos.js
+++ frontend/src/components/acontecimento/HistoricoAcontecimentos.js
@@ -1,7 +1,6 @@
-// src/components/acontecimento/HistoricoAcontecimentos.js
 import React, { useState, useEffect } from 'react';
-import { Table, Pagination, Form, Button, Container, Row, Col, Dropdown, DropdownButton, Modal } from 'react-bootstrap';
-import { fetchAcontecimentos } from '../../services/acontecimento/acontecimentoService';
+import { Table, Pagination, Form, Button, Container, Row, Col, Dropdown, DropdownButton, Modal, Alert } from 'react-bootstrap';
+import { fetchAcontecimentos, deleteAcontecimento, updateAcontecimento } from '../../services/acontecimento/acontecimentoService';
 import './HistoricoAcontecimentos.module.css';
 
 const HistoricoAcontecimentos = () => {
@@ -13,6 +12,8 @@ const HistoricoAcontecimentos = () => {
   const [totalPages, setTotalPages] = useState(1);
   const [showModal, setShowModal] = useState(false);
   const [selectedAcontecimento, setSelectedAcontecimento] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [deleteMessage, setDeleteMessage] = useState('');
 
   const loadAcontecimentos = async (page = 1) => {
     try {
@@ -38,8 +39,9 @@ const HistoricoAcontecimentos = () => {
     loadAcontecimentos(pageNumber);
   };
 
-  const handleShowModal = (acontecimento) => {
+  const handleShowModal = (acontecimento, editing = false) => {
     setSelectedAcontecimento(acontecimento);
+    setIsEditing(editing);
     setShowModal(true);
   };
 
@@ -48,11 +50,34 @@ const HistoricoAcontecimentos = () => {
     setSelectedAcontecimento(null);
   };
 
+  const handleDelete = async (id) => {
+    try {
+      await deleteAcontecimento(id);
+      setAcontecimentos(acontecimentos.filter(acontecimento => acontecimento._id !== id));
+      setDeleteMessage('Atendimento excluído com sucesso!');
+      setTimeout(() => setDeleteMessage(''), 3000);
+    } catch (error) {
+      console.error('Erro ao deletar acontecimento:', error);
+    }
+  };
+
+  const handleSave = async () => {
+    try {
+      await updateAcontecimento(selectedAcontecimento._id, selectedAcontecimento);
+      loadAcontecimentos(currentPage);
+      setIsEditing(false);
+      setShowModal(false);
+    } catch (error) {
+      console.error('Erro ao atualizar acontecimento:', error);
+    }
+  };
+
   return (
     <Container className="bd">
       <h3 className="my-3 text-left">Histórico de Acontecimentos</h3>
       <hr className="mb-4" />
       <h5>Filtros</h5>
+      {deleteMessage && <Alert variant="success">{deleteMessage}</Alert>}
       <Form onSubmit={handleSearch} className="mb-4">
         <Row>
           <Col md={3}>
@@ -118,9 +143,9 @@ const HistoricoAcontecimentos = () => {
               <td className="text-center">{`${acontecimento.rua}, ${acontecimento.bairro}, ${acontecimento.cidade}, ${acontecimento.estado}`}</td>
               <td className="text-center">
                 <DropdownButton id="dropdown-basic-button" title="Opções" className="acao-button" style={{ backgroundColor: '#78A6C7'}}>
-                  <Dropdown.Item onClick={() => handleShowModal(acontecimento)}>Visualizar</Dropdown.Item>
-                  <Dropdown.Item href="#/editar">Editar</Dropdown.Item>
-                  <Dropdown.Item href="#/deletar">Deletar</Dropdown.Item>
+                  <Dropdown.Item onClick={() => handleShowModal(acontecimento, false)}>Visualizar</Dropdown.Item>
+                  <Dropdown.Item onClick={() => handleShowModal(acontecimento, true)}>Editar</Dropdown.Item>
+                  <Dropdown.Item onClick={() => handleDelete(acontecimento._id)}>Deletar</Dropdown.Item>
                 </DropdownButton>
               </td>
             </tr>
@@ -144,28 +169,175 @@ const HistoricoAcontecimentos = () => {
       {selectedAcontecimento && (
         <Modal show={showModal} onHide={handleCloseModal} centered>
           <Modal.Header closeButton>
-            <Modal.Title>Informações do Atendimento</Modal.Title>
+            <Modal.Title>{isEditing ? "Editar Atendimento" : "Informações do Atendimento"}</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <div className="modal-content">
+            <div className="modal-content no-border">
               <div className="acontecimento">
                 <h5>Acontecimento</h5>
-                <p><strong>Subgrupo:</strong> {selectedAcontecimento.subgrupo}</p>
-                <p><strong>Tipo:</strong> {selectedAcontecimento.tipo}</p>
-                <p><strong>Evento:</strong> {selectedAcontecimento.evento}</p>
-                <p><strong>COBRADE:</strong> {selectedAcontecimento.infoCobrade}</p>
+                {isEditing ? (
+                  <>
+                    <Form.Group controlId="formClasse">
+                      <Form.Label>Classe</Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="classe"
+                        value={selectedAcontecimento.classe}
+                        onChange={(e) => setSelectedAcontecimento({ ...selectedAcontecimento, classe: e.target.value })}
+                      />
+                    </Form.Group>
+                    <Row>
+                      <Col md={6}>
+                        <Form.Group controlId="formGrupo">
+                          <Form.Label>Grupo</Form.Label>
+                          <Form.Control
+                            type="text"
+                            name="grupo"
+                            value={selectedAcontecimento.grupo}
+                            onChange={(e) => setSelectedAcontecimento({ ...selectedAcontecimento, grupo: e.target.value })}
+                          />
+                        </Form.Group>
+                      </Col>
+                      <Col md={6}>
+                        <Form.Group controlId="formSubgrupo">
+                          <Form.Label>Subgrupo</Form.Label>
+                          <Form.Control
+                            type="text"
+                            name="subgrupo"
+                            value={selectedAcontecimento.subgrupo}
+                            onChange={(e) => setSelectedAcontecimento({ ...selectedAcontecimento, subgrupo: e.target.value })}
+                          />
+                        </Form.Group>
+                      </Col>
+                      <Col md={6}>
+                        <Form.Group controlId="formTipo">
+                          <Form.Label>Tipo</Form.Label>
+                          <Form.Control
+                            type="text"
+                            name="tipo"
+                            value={selectedAcontecimento.tipo}
+                            onChange={(e) => setSelectedAcontecimento({ ...selectedAcontecimento, tipo: e.target.value })}
+                          />
+                        </Form.Group>
+                      </Col>
+                      <Col md={6}>
+                        <Form.Group controlId="formSubtipo">
+                          <Form.Label>Subtipo</Form.Label>
+                          <Form.Control
+                            type="text"
+                            name="subtipo"
+                            value={selectedAcontecimento.subtipo}
+                            onChange={(e) => setSelectedAcontecimento({ ...selectedAcontecimento, subtipo: e.target.value })}
+                          />
+                        </Form.Group>
+                      </Col>
+                      <Col md={6}>
+                        <Form.Group controlId="formEvento">
+                          <Form.Label>Evento</Form.Label>
+                          <Form.Control
+                            type="text"
+                            name="evento"
+                            value={selectedAcontecimento.evento}
+                            onChange={(e) => setSelectedAcontecimento({ ...selectedAcontecimento, evento: e.target.value })}
+                          />
+                        </Form.Group>
+                      </Col>
+                      <Col md={6}>
+                        <Form.Group controlId="formCobrade">
+                          <Form.Label>COBRADE</Form.Label>
+                          <Form.Control
+                            type="text"
+                            name="infoCobrade"
+                            value={selectedAcontecimento.infoCobrade}
+                            onChange={(e) => setSelectedAcontecimento({ ...selectedAcontecimento, infoCobrade: e.target.value })}
+                          />
+                        </Form.Group>
+                      </Col>
+                    </Row>
+                  </>
+                ) : (
+                  <>
+                    <p><strong>Classe:</strong> {selectedAcontecimento.classe}</p>
+                    <Row>
+                      <Col md={6}>
+                        <p><strong>Grupo:</strong> {selectedAcontecimento.grupo}</p>
+                        <p><strong>Subgrupo:</strong> {selectedAcontecimento.subgrupo}</p>
+                        <p><strong>Tipo:</strong> {selectedAcontecimento.tipo}</p>
+                      </Col>
+                      <Col md={6}>
+                        <p><strong>Subtipo:</strong> {selectedAcontecimento.subtipo}</p>
+                        <p><strong>Evento:</strong> {selectedAcontecimento.evento}</p>
+                        <p><strong>COBRADE:</strong> {selectedAcontecimento.infoCobrade}</p>
+                      </Col>
+                    </Row>
+                  </>
+                )}
                 <p><strong>Nº do protocolo:</strong> {selectedAcontecimento.numeroProtocolo}</p>
               </div>
               <div className="atendimento">
                 <h5>Atendimento</h5>
                 <p><strong>Nome do cidadão:</strong> {selectedAcontecimento.cidadaoResponsavel}</p>
-                <p><strong>CPF:</strong> {selectedAcontecimento.cpf}</p>
-                <p><strong>Endereço da ocorrência:</strong> {`${selectedAcontecimento.rua}, ${selectedAcontecimento.bairro}, ${selectedAcontecimento.cidade}, ${selectedAcontecimento.estado}`}</p>
+                {isEditing ? (
+                  <Row>
+                    <Col md={6}>
+                      <Form.Group controlId="formRua">
+                        <Form.Label>Endereço da ocorrência</Form.Label>
+                        <Form.Control
+                          type="text"
+                          name="rua"
+                          value={selectedAcontecimento.rua}
+                          onChange={(e) => setSelectedAcontecimento({ ...selectedAcontecimento, rua: e.target.value })}
+                        />
+                      </Form.Group>
+                    </Col>
+                    <Col md={6}>
+                      <Form.Group controlId="formBairro">
+                        <Form.Label>Bairro</Form.Label>
+                        <Form.Control
+                          type="text"
+                          name="bairro"
+                          value={selectedAcontecimento.bairro}
+                          onChange={(e) => setSelectedAcontecimento({ ...selectedAcontecimento, bairro: e.target.value })}
+                        />
+                      </Form.Group>
+                    </Col>
+                    <Col md={6}>
+                      <Form.Group controlId="formCidade">
+                        <Form.Label>Cidade</Form.Label>
+                        <Form.Control
+                          type="text"
+                          name="cidade"
+                          value={selectedAcontecimento.cidade}
+                          onChange={(e) => setSelectedAcontecimento({ ...selectedAcontecimento, cidade: e.target.value })}
+                        />
+                      </Form.Group>
+                    </Col>
+                    <Col md={6}>
+                      <Form.Group controlId="formEstado">
+                        <Form.Label>Estado</Form.Label>
+                        <Form.Control
+                          type="text"
+                          name="estado"
+                          value={selectedAcontecimento.estado}
+                          onChange={(e) => setSelectedAcontecimento({ ...selectedAcontecimento, estado: e.target.value })}
+                        />
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                ) : (
+                  <>
+                    <p><strong>Endereço da ocorrência:</strong> {`${selectedAcontecimento.rua}, ${selectedAcontecimento.bairro}, ${selectedAcontecimento.cidade}, ${selectedAcontecimento.estado}`}</p>
+                  </>
+                )}
               </div>
             </div>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={handleCloseModal}>Fechar</Button>
+            {isEditing ? (
+              <Button variant="primary" onClick={handleSave}>Salvar</Button>
+            ) : (
+              <Button variant="secondary" onClick={handleCloseModal}>Fechar</Button>
+            )}
           </Modal.Footer>
         </Modal>
       )}
--- frontend/src/services/acontecimento/acontecimentoService.js
+++ frontend/src/services/acontecimento/acontecimentoService.js
@@ -1,6 +1,6 @@
-// src/services/acontecimento/acontecimentoService.js
 import api from '../api';
 
+// Função para buscar acontecimentos
 export const fetchAcontecimentos = async (searchTerm, currentPage, limit = 10, startDate, endDate) => {
   try {
     const response = await api.get('/acontecimentos/search', {
@@ -18,6 +18,7 @@ export const fetchAcontecimentos = async (searchTerm, currentPage, limit = 10, s
   }
 };
 
+// Função para criar um acontecimento
 export const postAcontecimento = async (acontecimento) => {
   try {
     const response = await api.post('/acontecimentos', acontecimento, {
@@ -29,5 +30,32 @@ export const postAcontecimento = async (acontecimento) => {
     return response.data;
   } catch (error) {
     throw new Error('Erro ao criar acontecimento: ' + error.message);
+  }
+};
+
+// Função para deletar um acontecimento
+export const deleteAcontecimento = async (id) => {
+  try {
+    const response = await api.delete(`/acontecimentos/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao deletar acontecimento:', error);
+    throw new Error('Erro ao deletar acontecimento: ' + error.message);
+  }
+};
+
+// Função para atualizar um acontecimento
+export const updateAcontecimento = async (id, updatedData) => {
+  try {
+    const response = await api.put(`/acontecimentos/${id}`, updatedData, {
+      headers: {
+        "Accept": "application/json",
+        "content-type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao atualizar acontecimento:', error);
+    throw new Error('Erro ao atualizar acontecimento: ' + error.message);
   }
 };
